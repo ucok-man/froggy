24b6d1a682714c70e8e7b021f633189f668dd609
Alvin Odhi N <ucokkocu411@gmail.com>
2025-10-12 20:52:29 +0700

    feat: add ast defintion

---
+++ internal/ast/ast.go
@@ -0,0 +1,57 @@
+package ast
+
+import (
+	"github.com/ucok-man/froggy/internal/token"
+)
+
+/* --------------------------- Defintion -------------------------- */
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+/* --------------------------- Root Node -------------------------- */
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+/* ---------------------- Let Statement Node ---------------------- */
+
+// Merepresentasikan let statement, misalnya: let x = 5;
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier // nama variabel (mis. "x")
+	Value Expression  // expression di sisi kanan (mis. literal 5 atau lainnya)
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+/* ------------------------ Identifier Node ----------------------- */
+
+// Merepresentasikan identifier (nama variabel)
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
