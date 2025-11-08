57041ccfa0863b92f9dabb5cdaf92a8f2b86a296
ucok-man <ucokkocu411@gmail.com>
2025-11-08 11:17:53 +0700

    feat: implement evaluator for ast integer literal

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/ucok-man/froggy/internal/evaluator"
 	"github.com/ucok-man/froggy/internal/lexer"
 	"github.com/ucok-man/froggy/internal/parser"
 )
@@ -33,8 +34,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
