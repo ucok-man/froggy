ec7cc0f05823d784ea633af68954edd201cfdbc6
ucok-man <ucokkocu411@gmail.com>
2025-11-08 11:25:03 +0700

    feat: implement evaluator for boolean

--- internal/evaluator/evaluator.go
+++ internal/evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"github.com/ucok-man/froggy/internal/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 // evaluator/evaluator.go
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
@@ -19,6 +24,9 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -30,4 +38,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
